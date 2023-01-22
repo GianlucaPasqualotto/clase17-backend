a306e780984e521ab2f1a0991fdcedc1bdefb03f
Gianluca Pasqualotto <pasqualottogianluca@gmail.com>
2023-01-22 16:22:05 -0300

    ruta home

--- src/server.js
+++ src/server.js
@@ -7,3 +7,7 @@ app.listen(PORT,()=>console.log(`Server listening on port ${8080}`));
 app.get("/",(req,res)=>{
     res.send("<h1>Versiones git</h1>")
 });
+
+app.get("/home",(req,res)=>{
+    res.send("<h1>Bienvenido</h1>")
+});
