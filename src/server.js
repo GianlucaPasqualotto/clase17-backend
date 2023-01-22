06b2dbbedc0dee70e626e56b5de895b065170eb2
Gianluca Pasqualotto <pasqualottogianluca@gmail.com>
2023-01-22 17:22:16 -0300

    port update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 const app = express();
 
-const PORT = 8080;
-app.listen(PORT,()=>console.log(`Server listening on port ${8080}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
 
 app.get("/",(req,res)=>{
     res.send("<h1>Versiones git</h1>")
